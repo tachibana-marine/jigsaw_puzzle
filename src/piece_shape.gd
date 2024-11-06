5085f82d0a05538e584eb5d43b7074ef1f1ab61e
tachibana-marine <d.ageha.sekaijyu1041@gmail.com>
2024-11-07 00:26:27 +0900

    Create polygon

--- src/piece_shape.gd
+++ src/piece_shape.gd
@@ -8,20 +8,35 @@ extends Node2D
         size = value
         queue_redraw()
 
-@export var dimple: Vector4i = Vector4i(-1, -1, -1, -1):
+@export var dimple: Vector4i = Vector4i(0, 0, 0, 0):
     get: return dimple
     set(value):
         dimple = value
         queue_redraw()
 
+@export var dimple_shape: PackedVector2Array = []:
+    get: return dimple_shape
+    set(value):
+        dimple_shape = value
+
+
+var vertices: PackedVector2Array = [Vector2(0, 0), Vector2(10, 0), Vector2(10, -20), Vector2(40, -20), Vector2(40, 0), Vector2(100, 0),
+    Vector2(100, 20), Vector2(80, 20), Vector2(80, 50), Vector2(100, 50), Vector2(100, 100),
+    Vector2(70, 100), Vector2(70, 120), Vector2(40, 120), Vector2(40, 100), Vector2(0, 100),
+    Vector2(0, 70), Vector2(20, 70), Vector2(20, 40), Vector2(0, 40)]
+
 var draw_log: String = "":
     get: return draw_log
 
+    
 func _draw() -> void:
     draw_log = ""
-    draw_rect(Rect2(0, 0, size.x, size.y), Color.RED)
-    draw_log += "base " + str(size.x) + "," + str(size.y) + "\n"
-    var dimple_radius = 15
-    var dimple_height = 35
-    draw_rect(Rect2(30 - dimple_radius, -dimple_height, 2 * dimple_radius, dimple_height), Color.RED)
-    draw_log += "dimple 30,0\n"
+    draw_polygon(vertices, [Color.WHITE])
+    # draw_rect(Rect2(0, 0, size.x, size.y), Color.WHITE)
+    # draw_log += "base " + str(size.x) + "," + str(size.y) + "\n"
+    # var dimple_radius = 15
+    # var dimple_height = 35
+    # draw_rect(Rect2(30 - dimple_radius, -dimple_height, 2 * dimple_radius, dimple_height), Color.WHITE)
+    # draw_log += "dimple 30,0\n"
+    # draw_rect(Rect2(30 - dimple_radius, 100 - dimple_height, 2 * dimple_radius, dimple_height), Color.BLACK)
+    # draw_log += "dimple 30,0\n"
--- test/test_piece_shape.gd
+++ test/test_piece_shape.gd
@@ -7,8 +7,12 @@ var piece_shape = null
 func before_each():
     piece_shape = add_child_autofree(piece_shape_script.new())
 
-func test_piece_shape_properties():
+func test_piece_dimple_properties():
     assert_property(piece_shape, "size", Vector2.ZERO, Vector2(100, 100))
-    assert_property(piece_shape, "dimple", Vector4i(-1, -1, -1, -1), Vector4i(10, -1, -1, 20))
-    await wait_frames(1)
-    assert_eq(piece_shape.draw_log, "base 100,100\ndimple 10,0\n")
+    assert_property(piece_shape, "dimple", Vector4i(0, 0, 0, 0), Vector4i(10, -20, 30, -30))
+    # 30x20
+    assert_property(piece_shape, "dimple_shape", PackedVector2Array([]), PackedVector2Array([Vector2(0, 0), Vector2(0, -20), Vector2(30, -20), Vector2(30, 0)]))
+    assert_eq(piece_shape.vertices, PackedVector2Array([Vector2(0, 0), Vector2(10, 0), Vector2(10, -20), Vector2(40, -20), Vector2(40, 0), Vector2(100, 0),
+    Vector2(100, 20), Vector2(80, 20), Vector2(80, 50), Vector2(100, 50), Vector2(100, 100),
+    Vector2(70, 100), Vector2(70, 120), Vector2(40, 120), Vector2(40, 100), Vector2(0, 100),
+    Vector2(0, 70), Vector2(20, 70), Vector2(20, 40), Vector2(0, 40)]))
