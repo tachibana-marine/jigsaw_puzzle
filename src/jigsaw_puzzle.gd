e075c718c0921e64744de0fdfd11080f84c54313
tachibana-marine <d.ageha.sekaijyu1041@gmail.com>
2024-11-17 20:09:44 +0900

    start working on jigsaw_puzzle

--- src/jigsaw_puzzle.gd
+++ src/jigsaw_puzzle.gd
@@ -1,5 +1,4 @@
 @tool
-
 extends Node2D
 class_name JigsawPuzzle
 
@@ -8,44 +7,35 @@ class_name JigsawPuzzle
         return texture
     set(value):
         texture = value
-        $Sprite.texture = value
-        _create_pieces(texture.get_size())
-        randomize_pieces()
 
-func _create_pieces(size: Vector2):
-    var step_x = (int)(size.x / 2)
-    var step_y = (int)(size.y / 2)
-    for y in range(2):
-        for x in range(2):
-            var piece = Sprite2D.new()
-            piece.vframes = 2
-            piece.hframes = 2
-            piece.frame_coords = Vector2(x, y)
-            piece.centered = false
-            piece.texture = texture
-            piece.position = Vector2(x * step_x, y * step_y)
-            $PieceHolder.add_child(piece)
+@export var split_dimension: Vector2i = Vector2i(1, 1):
+    get: return split_dimension
+    set(value):
+        if (value.x >= 1 && value.y >= 1):
+            split_dimension = value
+            _create_pieces()
 
 func get_pieces():
     return $PieceHolder.get_children()
 
-func randomize_pieces():
-    var x = range(2)
-    var y = range(2)
-    x.shuffle()
-    y.shuffle()
-    var index = 0
-    for j in range(2):
-        for i in range(2):
-            get_pieces()[index].frame_coords = Vector2i(x[i], y[j])
-            index += 1
+func _create_pieces():
+    var piece_width = texture.get_size().x / split_dimension.x
+    var piece_height = texture.get_size().y / split_dimension.y
+    for child in $PieceHolder.get_children():
+        child.queue_free()
+    for j in range(split_dimension.y):
+        for i in range(split_dimension.x):
+            var piece = Piece.new()
+            piece.size = Vector2(piece_width, piece_height)
+            piece.texture = texture
+            piece.image_offset = -Vector2(piece_width * i, piece_height * j)
+            piece.position = Vector2((piece_width + 2) * i, (piece_height + 2) * j)
+            $PieceHolder.add_child(piece)
 
 func _init():
     var sprite = Sprite2D.new()
     sprite.name = "Sprite"
     sprite.hide()
-    # sprite.region_enabled = true
-    # sprite.region_rect = Rect2(100, 100, 200, 200)
     add_child(sprite)
 
     var piece_holder = Node2D.new()
--- test/test_jigsaw_puzzle.gd
+++ test/test_jigsaw_puzzle.gd
@@ -1,7 +1,7 @@
 extends GutTest
 
-var jigsaw_puzzle_script = load("res://src/jigsaw_puzzle.gd")
 var jigsaw_puzzle = null
+var piece_holder = null
 
 func create_empty_image_texture(width: int, height: int):
     var image = Image.create_empty(width, height, false, Image.FORMAT_RGB8)
@@ -9,22 +9,35 @@ func create_empty_image_texture(width: int, height: int):
     
 
 func before_each():
-    jigsaw_puzzle = autofree(jigsaw_puzzle_script.new())
+    jigsaw_puzzle = autofree(JigsawPuzzle.new())
+    piece_holder = jigsaw_puzzle.get_node("PieceHolder")
+
+func test_can_split_image():
+    jigsaw_puzzle.texture = create_empty_image_texture(15, 15)
+    assert_property(jigsaw_puzzle, "split_dimension", Vector2i(1, 1), Vector2i(3, 3))
+    var pieces = jigsaw_puzzle.get_pieces()
+    assert_eq(pieces.size(), 9)
+    assert_eq(pieces[0].size, Vector2(5, 5))
+    assert_eq(pieces[0].texture, jigsaw_puzzle.texture)
+    assert_eq(pieces[4].image_offset, Vector2(-5, -5))
+    assert_eq(pieces[4].position, Vector2(7, 7))
+    assert_eq(pieces[5].position, Vector2(14, 7))
+
     
+func test_split_dim_must_be_greater_than_zero():
+    jigsaw_puzzle.split_dimension = Vector2i(0, 0)
+    assert_eq(jigsaw_puzzle.split_dimension, Vector2i(1, 1))
+    jigsaw_puzzle.split_dimension = Vector2i(2, 0)
+    assert_eq(jigsaw_puzzle.split_dimension, Vector2i(1, 1))
+    jigsaw_puzzle.split_dimension = Vector2i(0, 2)
+    assert_eq(jigsaw_puzzle.split_dimension, Vector2i(1, 1))
+
 func test_can_set_texture():
     var image_texture = create_empty_image_texture(10, 10)
     assert_property(jigsaw_puzzle, "texture", null, image_texture)
 
-func test_get_4_pieces_arranged_randomly():
-    seed(1)
-    jigsaw_puzzle.texture = create_empty_image_texture(10, 10)
-    var pieces = jigsaw_puzzle.get_pieces()
-    assert_eq(pieces.size(), 4)
-    var i = 0
-    var displaced_count = 0
-    for y in range(2):
-        for x in range(2):
-            if (pieces[i].frame_coords != Vector2i(x, y)):
-                displaced_count += 1
-            i += 1
-    assert_ne(displaced_count, 0)
+# func test_can_change_texture_to_null():
+#     var image_texture = create_empty_image_texture(10, 10)
+#     assert_property(jigsaw_puzzle, "texture", null, image_texture)
+#     jigsaw_puzzle.texture = null
+#     assert_null(jigsaw_puzzle.texture)
